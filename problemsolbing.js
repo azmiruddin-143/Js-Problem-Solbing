6efc25eadf7ef395dab1f3bb9a14f972a941e0de
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-07 21:16:42 +0600

    first commit

---
+++ problemsolbing.js
@@ -0,0 +1,114 @@
+
+// node problemsolbing.js
+
+
+//  function num (num1,num2) {
+//     let gun = num1 * num2;
+//     console.log(gun);
+//  }
+//  num(26,9);
+
+
+// node problemsolbing.js
+
+// function number (num1, num2,most) {
+//        let gun = num1 * num2;
+//        let somadan = gun / most;
+//        console.log(somadan);
+
+// }
+// let first = 25;
+// let second = 66;
+// let therd = 100;
+// number(first,second,therd);
+
+
+
+// node problemsolbing.js//
+
+// even valu 2 dara gun hobe and odd valu gula 30 sate bag hobe
+// function az() {
+//     for (let soni of man) {
+//         if (soni % 2 === 0) {
+//             let first = soni * 2;
+//             console.log("first er value", first);
+//         } else if (soni % 2 === 1) {
+//             let second = soni / 30;
+//             console.log("first er value", second);
+//         } else {
+
+//             console.log(first - second, "sob bul");
+//         }
+//     }
+// }
+
+// let man = [5, 265, 451, 50, 69, 8, 66, 99, 120]
+// az()
+
+
+// node problemsolbing.js//
+
+// function hi(man) {
+//     let kire = [];
+//     for (let fi of man) {
+//         if (fi % 2 === 1) {
+//             kire.push(fi);
+//         }
+//     }
+
+//     let sum = 0;
+
+//     for (let bib of kire) {
+//         sum = sum+bib;
+//     }
+//     let len = kire.length;
+//     let funaly = sum / len
+//    console.log(funaly);
+
+// }
+
+// let man = [5, 10, 60, 88, 99, 9, 77, 120]
+// hi(man)
+
+
+
+// node problemsolbing.js//
+// let person = ["Azmir", "soniya", "Asif", "nawus", "soniya", "Asif"]
+// let number = [54, 415, 566, , 66, 415, 90, 566, 20, 54]
+
+// function onename (arry) {
+//     let incts = []
+//     for(let item of arry){
+//         if(incts.includes(item)===false){
+//             incts.push(item)
+//         }
+//     }
+
+//     return incts
+// }
+
+//  let incluts = onename(person)
+// console.log(incluts);
+
+
+
+// let choto = Math.min(45,1456,4,5416,1564156,6,15,15,21,);
+// console.log(choto);
+
+
+// let boro = Math.max(45, 1456, 4, 5416, 1556, 6, 15, 15, 21,);
+// console.log(boro);
+
+// node problemsolbing.js//
+
+
+
+// let random = Math.round(Math.random()*20)
+// console.log(random)
+
+//  a = 10;
+//  b= 55;
+//   let man = a;
+//   a = b;
+//   b = man
+//  console.log(a,b);
